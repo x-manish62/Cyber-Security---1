eae0f0b47395d2015629fa07428942f2ada887b9
Manish <yadavmanishmky2004@gmail.com>
2026-03-18 16:51:02 +0530

    Initialized

---
+++ script.js
@@ -0,0 +1,23 @@
+function login(){
+
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+if(user != "" && pass != ""){
+
+window.location="dashboard.html";
+
+}
+else{
+
+alert("Enter Username and Password");
+
+}
+
+}
+
+function logout(){
+
+window.location="index.html";
+
+}
